ffea7fb4c7dcf385982e99cc2be6d04d2bb9f407
NatashaLebed <Natasha.lebed@gmail.com>
2013-10-29 15:13:36 +0200

    namespace

--- index.php
+++ index.php
@@ -1,15 +1,28 @@
 <?php
 
+namespace natasha;
+
 require_once 'vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-$request = Request::createFromGlobals();
-$uri1 = $request->getPathInfo();
+$mySummer = new SummerPerambulator('Chikko','7','3');
+echo $mySummer->priceTitle().'<br>';
+echo 'Fold - '. $mySummer->fold() .'<br><br>';
 
+$myTransformer = new TransformerPerambulator('GoodBaby','15','4');
+echo $myTransformer->priceTitle().'<br>';
+echo 'Fold - '. $myTransformer->fold().'<br>';
+echo 'ProtectCold - '. $myTransformer->protectCold().'<br>';
+echo 'ReversibleHandle - '. $myTransformer->reversibleHandle().'<br>';
+
+
+echo '<br>';
+echo '<br>';
+echo '<br>';
 $response = new Response();
-$response->setContent('SummerPerambulator');
+$response->setContent($myTransformer->priceTitle());
 $response->setStatusCode(200);
 $response->headers->set('Content-Type', 'text/html');
 $response->send();
