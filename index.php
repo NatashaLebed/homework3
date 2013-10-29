1b860902c0412e169351c8a7c863c4ea3c02bef1
NatashaLebed <Natasha.lebed@gmail.com>
2013-10-29 14:12:30 +0200

    symfonyHttpFoundation

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
-$uri = $_SERVER['REQUEST_URI'];
-$foo = $_GET['foo'];
 
-header('Content-type: text/html');
-echo 'The Urirequested is:'.$uri;
-echo 'The value of the "foo" parameter is'.$foo;
+require_once 'vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+$uri1 = $request->getPathInfo();
+
+$response = new Response();
+$response->setContent('SummerPerambulator');
+$response->setStatusCode(200);
+$response->headers->set('Content-Type', 'text/html');
+$response->send();
